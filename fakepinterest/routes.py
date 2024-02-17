fea9462bc01897eb738e7a1af47932323caadade
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-16 23:18:19 -0300

    login-logout + static

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,22 +1,49 @@
-from flask import render_template, url_for
-from fakepinterest import app
-from flask_login import login_required
+from flask import render_template, url_for, redirect
+from fakepinterest import app, database, bcrypt
+from fakepinterest.models import Usuario, Foto
+from flask_login import login_required, login_user, logout_user, current_user
 from fakepinterest.forms import FormLogin, FormCriarConta
 
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    formlogin = FormLogin()
-    return render_template("homepage.html", form=formlogin)
+    form_login = FormLogin()
+    if form_login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario)
+            return redirect(url_for("perfil", id_usuario=usuario.id))    
+    return render_template("homepage.html", form=form_login)
 
 
 @app.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
     form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data, 
+                        email=form_criarconta.email.data, senha=senha)
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template("criarconta.html", form=form_criarconta)
 
 
-@app.route("/perfil/<usuario>")
+@app.route("/perfil/<id_usuario>")
 @login_required
-def perfil(usuario):
-    return render_template("perfil.html", usuario=usuario)
+def perfil(id_usuario):
+    if int(id_usuario) == int(current_user.id):
+        #usuário vendo o próprio perfil
+        return render_template("perfil.html", usuario=current_user)
+    else:
+        # vendo o perfil de outra pessoa
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template("perfil.html", usuario=id_usuario)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
