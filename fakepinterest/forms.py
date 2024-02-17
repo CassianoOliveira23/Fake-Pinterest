9d6c258c6403dc0097439169c946cbd3e802ce77
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-16 21:31:58 -0300

    Forms

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,23 @@
-# criar os formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+# create forms
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de Usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(8, 20)])
+    confirmacao_senha = PasswordField("Confirmar Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+    
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("Email já cadastrado. Faça login para continuar")
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,10 +1,20 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    form_criarconta = FormCriarConta()
+    return render_template("criarconta.html", form=form_criarconta)
+
 
 @app.route("/perfil/<usuario>")
 @login_required
