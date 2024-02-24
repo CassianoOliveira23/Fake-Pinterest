dbd0974a5796552860e6bd7b6c680b61b040ca8b
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-24 16:00:21 -0300

    sqlalchemy

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://projetoflask_db_user:GVB45fgLcPycsvYYmZpR72KZDESyLMB3@dpg-cnd37l7109ks738qqcv0-a/projetoflask_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "9944a393393d275f3a9e27be64a64ca3"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
