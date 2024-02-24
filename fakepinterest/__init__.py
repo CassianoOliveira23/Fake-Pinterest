6ea7f6af9028fe269d2c947ade9b84a7b4e327cf
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-24 15:36:43 -0300

    biblioteca psycopg2

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "9944a393393d275f3a9e27be64a64ca3"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
