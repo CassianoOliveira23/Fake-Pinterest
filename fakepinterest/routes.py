e42bf7e5e2912be0d8c40c589224add967e94516
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-24 16:26:05 -0300

    ajuste login

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -14,7 +14,7 @@ def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode("utf-8"), form_login.senha.data):
             login_user(usuario)
             return redirect(url_for("perfil", id_usuario=usuario.id))    
     return render_template("homepage.html", form=form_login)
@@ -25,7 +25,7 @@ def homepage():
 def criarconta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
-        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criarconta.username.data, 
                         email=form_criarconta.email.data, senha=senha)
         database.session.add(usuario)
